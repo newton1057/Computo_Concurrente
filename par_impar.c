9fdc70700792c18e9f4b49336c61859fe705c47d
newton1057 <newton1057@gmail.com>
2024-08-20 14:42:06 -0600

    PAR IMPAR MPI

---
+++ par_impar.c
@@ -0,0 +1,34 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    /* No debe haber llamadas MPI antes de invocar Init */
+    MPI_Init(&argc, &argv);
+
+    int miRank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &miRank);
+
+    int numProcs; 
+    MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
+
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+
+    MPI_Get_processor_name(processor_name, &name_len);
+
+    // Código que ejecutarán los procesos
+    //printf("\nHola mundo. Yo soy el proceso %d de %d procesos y me llamo %s \n",miRank, numProcs,processor_name);
+    if (miRank % 2 == 0 ){
+        printf("Soy el proceso %d, asi que soy PAR \n",miRank);
+    }else{
+        printf("Soy el proceso %d, asi que soy IMPAR \n",miRank);
+    }
+    
+
+    MPI_Finalize();
+
+    /* No debe haber llamadas MPI después de esto. */
+    return 0;
+}
+
